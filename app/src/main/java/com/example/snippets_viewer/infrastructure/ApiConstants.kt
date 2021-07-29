97818b0172ea2fc5a3edda85ec993bf9cada3ee4
JBertho <james.bertho94@gmail.com>
2021-07-29 09:41:17 +0200

    set public api url

--- app/src/main/java/com/example/snippets_viewer/infrastructure/ApiConstants.kt
+++ app/src/main/java/com/example/snippets_viewer/infrastructure/ApiConstants.kt
@@ -1,7 +1,7 @@
 package com.example.snippets_viewer.infrastructure
 
 object ApiConstants {
-    const val BaseUrl = "http://10.0.2.2:8080/api/"
+    const val BaseUrl = "http://20.199.104.235:8080/api/"
 
     object EndPoints {
         const val Auth = "auth/"
